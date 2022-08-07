6cdc7c77dde0e00132eec5a090bd8c2a98d40d2c
Wuchieh <admin@wuchieh.com>
2022-08-07 12:51:52 +0800

    init project

--- main.go
+++ main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
+	err := r.SetTrustedProxies([]string{"127.0.0.1"})
+	if err != nil {
+		log.Panicln("Error:", err.Error())
+	}
+
+	r.GET("/status", status)
+	r.GET("/", index)
+
+	err = r.Run("127.0.0.1:8080")
+	if err != nil {
+		log.Panicln("Error:", err.Error())
+	}
+}
+
+func index(c *gin.Context) {
+	fmt.Println("index")
+	c.HTML(http.StatusOK, "index.html", gin.H{
+		"message": "載入完成",
+	})
+}
+
+func status(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status":  "200",
+		"message": "server Online",
+	})
 }
