4c9a8fb8a68378b5e215138c0e2d3d09f1f069f2
Wuchieh <admin@wuchieh.com>
2022-08-07 16:42:28 +0800

    新增模板Function

--- main.go
+++ main.go
@@ -3,31 +3,43 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
+var (
+	r = gin.Default()
+)
+
+func formatAsDate(t time.Time) string {
+	year, month, day := t.Date()
+	return fmt.Sprintf("%d/%02d/%02d", year, month, day)
+}
+
 func main() {
-	r := gin.Default()
-	r.LoadHTMLGlob("templates/*")
-	err := r.SetTrustedProxies([]string{"127.0.0.1"})
-	if err != nil {
-		log.Panicln("Error:", err.Error())
-	}
+
+	r.SetFuncMap(template.FuncMap{
+		"formatAsDate": formatAsDate,
+	})
+
+	// 載入順序很重要 若先載入template會因為沒有FuncMap報錯
+	r.LoadHTMLGlob("./templates/*")
 
 	r.GET("/status", status)
 	r.GET("/", index)
 
-	err = r.Run("127.0.0.1:8080")
+	err := r.Run(":8080")
 	if err != nil {
 		log.Panicln("Error:", err.Error())
 	}
 }
 
 func index(c *gin.Context) {
-	fmt.Println("index")
-	c.HTML(http.StatusOK, "index.html", gin.H{
+	c.HTML(http.StatusOK, "index.html", map[string]interface{}{
 		"message": "載入完成",
+		"now":     time.Now(),
 	})
 }
 
